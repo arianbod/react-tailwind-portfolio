b183aae03c11c660b58c00d8c9b604c1cdbfbae1
Arian <Arianbod@outlook.com>
2024-02-18 23:37:37 +0300

    Information added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,24 +3,37 @@ import HeroImg from '../assets/hero.svg';
 import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from 'react-icons/fa';
 const Hero = () => {
 	return (
-		<div className='bg-emerald-100 py-24'>
+		<div className='bg-emerald-100 py-24 flex'>
 			<div className='mx-auto max-w-7xl px-8 grid md:grid-cols-2 items-center gap-8'>
 				<article>
 					<h1 className='text-7xl font-bold tracking-wider text-gray-700'>
 						I'm Arian
 					</h1>
 					<p className='mt-4 text-3xl text-slate-700 capitalize tracking-wide'>
-						{' '}
 						Front-End Developer
 					</p>
-					<p className='mt-2 text-lg text-slate-700'></p>
+					<p className='mt-2 text-lg text-slate-700 capitalize tracking-wide'>
+						Turning ideas into interactive reality
+					</p>
+					<div className='flex gap-x-4 mt-4'>
+						<a href='#'>
+							<FaGithubSquare className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
+						</a>
+						<a href='#'>
+							<FaLinkedin className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
+						</a>
+						{/* <a href='#'>
+							<FaTwitterSquare className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
+						</a> */}
+					</div>
 				</article>
-			</div>
-			<div className='image-container '>
-				<img
-					src={HeroImg}
-					alt='HeroImage'
-				/>
+				<article className='image-container hidden md:block'>
+					<img
+						src={HeroImg}
+						alt='HeroImage'
+						className='h-80 lg:h-96'
+					/>
+				</article>
 			</div>
 		</div>
 	);
