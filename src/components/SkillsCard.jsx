7a24ca982809388262ebfee284a6c48a45cda501
Arian <Arianbod@outlook.com>
2024-02-19 01:25:47 +0300

    created

--- src/components/SkillsCard.jsx
+++ src/components/SkillsCard.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const SkillsCard = ({ id, title, icon, text }) => {
-	return <div>{title}</div>;
+	return (
+		<article>
+			<span>{icon}</span>
+			<h4 className='font-bold mt-6'>{title}</h4>
+			<p className='mt-2 text-slate-500'>{text}</p>
+		</article>
+	);
 };
 
 export default SkillsCard;
