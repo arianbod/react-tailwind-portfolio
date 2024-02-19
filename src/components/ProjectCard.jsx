c18421135cf664a095e650d6d724b2282cfb2cf2
Arian <Arianbod@outlook.com>
2024-02-20 01:47:41 +0300

    if added

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,10 +1,10 @@
 import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from 'react-icons/fa';
 import { TbWorldWww } from 'react-icons/tb';
-const ProjectsCard = ({ url, image, git, title, text = 'default' }) => {
+const ProjectsCard = ({ url, img, git, title, text }) => {
 	return (
 		<article className='bg-white rounded-lg shadow-md block hover:shadow-xl duration-300'>
 			<img
-				src={image}
+				src={img}
 				alt={title}
 				className='w-full object-cover rounded-t-lg h-64 '
 			/>
@@ -12,12 +12,22 @@ const ProjectsCard = ({ url, image, git, title, text = 'default' }) => {
 				<h2 className='text-xl tracking-wide font-medium'>{title}</h2>
 				<p className='mt-4 text-slate-700 leading-loose'>{text}</p>
 				<div className='mt-4 flex gap-x-4'>
-					<a href={url}>
-						<TbWorldWww className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
-					</a>
-					<a href={git}>
-						<FaGithubSquare className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
-					</a>
+					{url && (
+						<a
+							href={url}
+							target='_blank'
+							rel='noreferrer'>
+							<TbWorldWww className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
+						</a>
+					)}
+					{git && (
+						<a
+							href={git}
+							target='_blank'
+							rel='noreferrer'>
+							<FaGithubSquare className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
+						</a>
+					)}
 				</div>
 			</div>
 		</article>
