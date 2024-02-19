736ae5b7d7d560ed2e2421b5b79171a712081d67
Arian <Arianbod@outlook.com>
2024-02-20 01:24:20 +0300

    State added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,24 @@
 import ProjectCard from './ProjectCard';
 import { projects } from '../data';
 import SectionTitle from './SectionTitle';
+import { useEffect, useState } from 'react';
 const Projects = () => {
+	const [projectData, setProjectData] = useState([...projects]);
+	const FetchAPI = async () => {
+        
+    };
+
+	useEffect(() => {
+		FetchAPI;
+	}, []);
+	console.log(projectData);
 	return (
 		<section
 			className='py-20 align-element'
 			id='projects'>
 			<SectionTitle text='web creations' />
 			<div className='py-16 grid lg:grid-cols-2 xl:grid-cols-3 gap-8'>
-				{projects.map((project) => {
+				{projectData.map((project) => {
 					return (
 						<ProjectCard
 							key={project.id}
